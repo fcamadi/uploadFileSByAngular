917d9b573c431d15a536e3cc6053653b2ca08598
Fran Camacho <fcamacho@serikat.es>
2020-02-26 11:46:26 +0100

    initial commit - projects created

---
+++ uploadFileBackend/src/main/java/org/fran/upload/UploadFileBackendApplication.java
@@ -0,0 +1,13 @@
+package org.fran.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadFileBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadFileBackendApplication.class, args);
+	}
+
+}
